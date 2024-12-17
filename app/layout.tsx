43d3668a49cd055e0aca7cd2288466e6c833211e
muathzoubi <moathaza@gmail.com>
2024-12-17 11:41:20 -0800

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,6 @@ import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
-import { TailwindIndicator } from "@/components/tailwind-indicator"
-import { ThemeProvider } from "@/components/theme-provider"
 
 export const metadata: Metadata = {
   title: {
@@ -40,13 +38,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
             fontSans.variable
           )}
         >
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="relative flex min-h-screen flex-col">
               <SiteHeader />
               <div className="flex-1">{children}</div>
             </div>
-            <TailwindIndicator />
-          </ThemeProvider>
         </body>
       </html>
     </>
--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Tools.js",
   description:
     "Beautifully designed components built with Radix UI and Tailwind CSS.",
   mainNav: [
@@ -12,6 +12,10 @@ export const siteConfig = {
     {
       title: "Dashboard",
       href: "/dashboard",
+    }, 
+    {
+      title: "Login",
+      href: "/login",
     },
   ],
   links: {
